de0eea61c7b3c7c07a4afddf6b1bb0803dee4558
ZPhreak <robert@raw-data.net>
2023-03-25 16:36:32 -0400

    End of Section 5

--- angularcore-ui/src/app/students/view-student/view-student.component.ts
+++ angularcore-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -30,9 +33,13 @@ export class ViewStudentComponent implements OnInit {
     },
   };
 
+  genderList: Gender[] = [];
+
   constructor(
     private readonly studentService: StudentService,
-    private readonly route: ActivatedRoute
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -45,7 +52,25 @@ export class ViewStudentComponent implements OnInit {
           .subscribe((successResponse) => {
             this.student = successResponse;
           });
+        this.genderService.getGenderList().subscribe((successResponse) => {
+          this.genderList = successResponse;
+        });
       }
     });
+  }
+
+  onUpdate(): void {
+    this.studentService.updateStudent(this.student.id, this.student).subscribe(
+      (successResponse) => {
+        // Show a notification
+        this.snackbar.open('Student updated successfully!', undefined, {
+          duration: 2000,
+        });
+      },
+      (errorResponse) => {
+        console.log(errorResponse);
+        // Log it
+      }
+    );
   }
 }
